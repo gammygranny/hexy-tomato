a7a39386b9ad126ea4b8f27b21210226b7c1f191
gammygranny <plumedarcher@tutanota.com>
2022-02-28 18:47:30 +0000

    aaaaaaaaaaaa

--- theclock.py
+++ theclock.py
@@ -6,19 +6,21 @@ from tkinter import *
 canvas = Tk()
 canvas.title("Digital Clock")
 canvas.geometry("350x200")
-canvas.resizable(2, 2)
+canvas.resizable(1, 1)
 label = Label(canvas, font=("Verdana", d(20), 'bold'), bg="blue", fg="white", bd=d(20))
-label.grid(row=2, column=2)
+label.grid(row=1, column=0,sticky=NE)
 pom = Label(canvas, font=("Verdana", d(40), 'bold'), bg="blue", fg="white", bd=d(40))
-
+pom.grid(row=0,column=0)
 def hextime():
     truetime = str(int(h(time.time() * (d(43) / d(122))) % 1000000))
-    return truetime[0:2], ':', truetime[2:4], ':', truetime[4:d(10)]
+    return truetime[0:2], ':', truetime[2:4]#, ':', truetime[4:d(10)]
 
 
 def digitalclock():
-    text_input = hextime() #time.strftime("%H:%M:%S")
-    label.config(text=text_input)
+    the_time = hextime() #time.strftime("%H:%M:%S")
+    label.config(text=the_time)
+    countdown = 'booba'
+    pom.config(text=countdown)
     label.after(200, digitalclock)
 
 
